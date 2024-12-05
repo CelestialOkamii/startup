cf4234d1bf4c374861051a4e0a1e65a940962250
CelestialOkamii <danielle.ebenal@outlook.com>
2024-12-05 12:17:50 -0800

    i thikn im on the right track, i found a website to help walk me through it

--- CSS/Startup/customization.js
+++ CSS/Startup/customization.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from 'react';
 function change_image_visability() {
     const [chosen_option, set_chosen_option] = usestate("");
     useEffect(() => {
-        fetch
+        fetch("customization.html")
+        .then((response) => response.text())
+        .then((html) => {
+            const dropdownContainer = document.getElementById("Hats");
+            const drop_downs = document.querySelectorAll("my_drop_down");
+            drop_downs.forEach((drop_down) => {})
+        })
     })
 }
