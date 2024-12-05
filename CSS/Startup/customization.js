3feb11d25799e64cc580a33caaa9438c7e03055f
CelestialOkamii <danielle.ebenal@outlook.com>
2024-12-05 13:18:55 -0800

    trying to make event listeners for each heading

--- CSS/Startup/customization.js
+++ CSS/Startup/customization.js
@@ -1,4 +1,19 @@
-import React, { useState, useEffect } from 'react';
-function change_image_visability() {
+document.getElementById("no_hat").addEventListener("click", make_thing_appear("nothing"));
+document.getElementById("party_hat").addEventListener("click", make_thing_appear("party_hat"));
+document.getElementById("ball_cap").addEventListener("click", make_thing_appear("ball_cap"));
+
+document.getElementById("no_face").addEventListener("click", make_thing_appear("nothing"));
+document.getElementById("monocle").addEventListener("click", make_thing_appear("monocle"));
+
+document.getElementById("no_neck").addEventListener("click", make_thing_appear("nothing"));
+document.getElementById("scarf").addEventListener("click", make_thing_appear("scarf"));
+document.getElementById("necklace").addEventListener("click", make_thing_appear("necklace"));
+
+document.getElementById("no_object").addEventListener("click", make_thing_appear("nothing"));
+document.getElementById("book").addEventListener("click", make_thing_appear("book"));
+document.getElementById("shopping_bag").addEventListener("click", make_thing_appear("shopping_bag"));
+
+
+function make_thing_appear(thing) {
     
 }
