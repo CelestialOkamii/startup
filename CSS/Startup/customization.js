a735cd4a8d6c435ab0ffe2acd68204e0c619301e
CelestialOkamii <danielle.ebenal@outlook.com>
2024-12-05 12:04:56 -0800

    trying to figure out how to snap html file into react doc

--- CSS/Startup/customization.js
+++ CSS/Startup/customization.js
@@ -1,1 +1,7 @@
 import React, { useState, useEffect } from 'react';
+function change_image_visability() {
+    const [chosen_option, set_chosen_option] = usestate("");
+    useEffect(() => {
+        fetch
+    })
+}
