2a0376f8d51df3c09f4b160eb2c5709b388bddde
CelestialOkamii <danielle.ebenal@outlook.com>
2024-12-06 15:26:30 -0800

    trying to make my code better so it will work

--- CSS/Startup/customization.js
+++ CSS/Startup/customization.js
@@ -1,24 +1,22 @@
 function get_option1() {
-    console.log('4')
-    selected = document.querySelector('Hats');
-    output = selected.options;
-    [selected.selectedIndex].values;
-    change_hat
+    let selected = document.querySelector('#Hats'); // Select the Hats dropdown
+    let selected_value = selected.options[selected.selectedIndex].value; // Get the selected value
+    change_hat(selected_value); // Call change_hat with the selected value
 }
 
+// Function to handle dropdown changes for Objects
 function get_option2() {
-    console.log('4')
-    selected = document.querySelector('Objects');
-    output = selected.options;
-    [selected.selectedIndex].values;
-    change_object
+    let selected = document.querySelector('#Objects'); // Select the Objects dropdown
+    let selected_value = selected.options[selected.selectedIndex].value; // Get the selected value
+    change_object(selected_value); // Call change_object with the selected value
 }
-
 
 
 function change_hat(hat) {
+     
     
 }
+document.getElementById("Hats").onchange = change_hat;
 
 function change_object(object) {
 
