9e8001c16b7340c28aa6db94d2f07f98364f0c74
CelestialOkamii <danielle.ebenal@outlook.com>
2024-12-06 11:21:39 -0800

    i think i made functions that can tell when a dropdown item is selected. going to try to link them to another function to make things appear and disappear

--- CSS/Startup/customization.js
+++ CSS/Startup/customization.js
@@ -1,17 +1,25 @@
+function get_option1() {
+    console.log('4')
+    selected = document.querySelector('Hats');
+    output = selected.options;
+    [selected.selectedIndex].values;
+    change_hat
+}
+
+function get_option2() {
+    console.log('4')
+    selected = document.querySelector('Objects');
+    output = selected.options;
+    [selected.selectedIndex].values;
+    change_object
+}
+
+
+
 function change_hat(hat) {
     
 }
-call function for each drop down using onchange and 
-selected in js 
-
-function change_face(face) {
-
-}
-
-function change_neck(neck) {
-
-}
 
 function change_object(object) {
-    
+
 }
