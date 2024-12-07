a347bbfbb7ec766a542f53fa7af1b64549f1cd6f
CelestialOkamii <danielle.ebenal@outlook.com>
2024-12-07 12:29:34 -0800

    got rid of fodder comments  i made when i was stuck

--- CSS/Startup/customization.js
+++ CSS/Startup/customization.js
@@ -1,15 +1,15 @@
 function get_option1() {
     console.log('IS IN GETOPTION1')
-    let selected = document.querySelector('#Hats'); // Select the Hats dropdown
-    let selected_value = selected.options[selected.selectedIndex].value; // Get the selected value
-    change_hat(selected_value); // Call change_hat with the selected value
+    let selected = document.querySelector('#Hats'); 
+    let selected_value = selected.options[selected.selectedIndex].value;
+    change_hat(selected_value); 
 }
 
 // Function to handle dropdown changes for Objects
 function get_option2() {
-    let selected = document.querySelector('#Objects'); // Select the Objects dropdown
-    let selected_value = selected.options[selected.selectedIndex].value; // Get the selected value
-    change_object(selected_value); // Call change_object with the selected value
+    let selected = document.querySelector('#Objects'); 
+    let selected_value = selected.options[selected.selectedIndex].value; 
+    change_object(selected_value); 
 }
 
 //Function to make things disappear and appear
